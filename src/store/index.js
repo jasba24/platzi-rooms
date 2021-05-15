8377a6030a555b5f58f78b57e00661c01b46e481
jasba24 <johanbuitrago342@gmail.com>
2021-05-15 17:42:33 -0500

    add modal

--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,18 @@ import { createStore } from "vuex"
 export default createStore({
 	state: {
 		user: null,
+		modals: {
+			login: false,
+		},
 	},
-	mutations: {},
-	actions: {},
-	modules: {},
+	mutations: {
+		SET_MODAL_STATE: (state, { name, value }) => {
+			state.modals[name] = value
+		},
+	},
+	actions: {
+		TOOGLE_MODAL_STATE: ({ commit }, { name, value }) => {
+			commit("SET_MODAL_STATE", { name, value })
+		},
+	},
 })
