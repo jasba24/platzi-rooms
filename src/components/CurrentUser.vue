2caecc9925bfcd4a417a4497a419f927f60c49fa
jasba24 <johanbuitrago342@gmail.com>
2021-05-29 15:41:32 -0500

    feat: :zap: add slotProp to compose userLogin

---
+++ src/components/CurrentUser.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="current-user">
+		<slot :user="user"></slot>
+	</div>
+</template>
+
+<script>
+import { mapGetters } from "vuex"
+
+export default {
+	name: "currentUser",
+
+	computed: {
+		...mapGetters({
+			user: "authUser",
+		}),
+	},
+}
+</script>
+
+<style></style>
--- src/partials/HeaderPartial.vue
+++ src/partials/HeaderPartial.vue
@@ -12,48 +12,58 @@
 				</div>
 				<div class="flex items-center w-auto">
 					<div class="items__controls">
-						<div class="flex" v-if="user">
-							<router-link
-								tag="button"
-								class="mr-2 flex items-center"
-								:to="{ name: 'CreateHousePage' }"
-							>
-								<font-awesome-icon icon="plus" class="text-black" />
-							</router-link>
-							<button class="mr-4 flex items-center">
-								<font-awesome-icon icon="bell" />
-							</button>
-							<div class="flex items-center">
-								<img
-									class="w-8 h-8 rounded-full mr-2"
-									src="https://avatars2.githubusercontent.com/u/1901273?s=460&v=4"
-									alt="Avatar of Javier Diaz"
-								/>
-								<div class="text-sm">
-									<router-link :to="{ name: 'ProfilePage' }" class="no-underline">
-										<p class="text-black leading-none">{{ user.name }}</p>
+						<CurrentUser>
+							<template v-slot="{ user }">
+								<div class="flex" v-if="user">
+									<router-link
+										tag="button"
+										class="mr-2 flex items-center"
+										:to="{ name: 'CreateHousePage' }"
+									>
+										<font-awesome-icon icon="plus" class="text-black" />
 									</router-link>
-									<p class="text-grey-dark">Online</p>
+									<button class="mr-4 flex items-center">
+										<font-awesome-icon icon="bell" />
+									</button>
+									<div class="flex items-center">
+										<img
+											class="w-8 h-8 rounded-full mr-2"
+											src="https://avatars2.githubusercontent.com/u/1901273?s=460&v=4"
+											alt="Avatar of Javier Diaz"
+										/>
+										<div class="text-sm">
+											<router-link
+												:to="{ name: 'ProfilePage' }"
+												class="no-underline"
+											>
+												<p class="text-black leading-none">{{ user.name }}</p>
+											</router-link>
+											<p class="text-grey-dark">Online</p>
+										</div>
+									</div>
+									<button
+										class="flex items-center ml-4"
+										@click.prevent="logOut"
+									>
+										<font-awesome-icon icon="power-off" />
+									</button>
 								</div>
-							</div>
-							<button class="flex items-center ml-4" @click.prevent="logOut">
-								<font-awesome-icon icon="power-off" />
-							</button>
-						</div>
-						<div v-else>
-							<button
-								class="btn__outline btn__outline--teal rounded mr-2"
-								@click.prevent="openModal('login')"
-							>
-								Login
-							</button>
-							<button
-								class="bg-yellow-dark text-yellow-darker font-semibold py-2 px-4 rounded"
-								@click.prevent="openModal('register')"
-							>
-								Register
-							</button>
-						</div>
+								<div v-else>
+									<button
+										class="btn__outline btn__outline--teal rounded mr-2"
+										@click.prevent="openModal('login')"
+									>
+										Login
+									</button>
+									<button
+										class="bg-yellow-dark text-yellow-darker font-semibold py-2 px-4 rounded"
+										@click.prevent="openModal('register')"
+									>
+										Register
+									</button>
+								</div>
+							</template>
+						</CurrentUser>
 					</div>
 				</div>
 			</div>
@@ -63,9 +73,12 @@
 
 <script>
 import { mapGetters } from "vuex"
+import CurrentUser from "../components/CurrentUser.vue"
 
 export default {
 	name: "HeaderPartial",
+
+	components: { CurrentUser },
 
 	data() {
 		return {
@@ -83,12 +96,6 @@ export default {
 		logOut() {
 			this.$store.dispatch("LOG_OUT")
 		},
-	},
-
-	computed: {
-		...mapGetters({
-			user: "authUser",
-		}),
 	},
 }
 </script>
